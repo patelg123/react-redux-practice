f5118f24698da10d093cc2d481eb7bfbe62f0fae
Girish Patel <patelg123@gmail.com>
2019-02-13 17:07:35 +0000

    Modified files

--- src/App.js
+++ src/App.js
@@ -1,27 +1,13 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import HelloWorld from "./components/presentation/HelloWorld";
 
 class App extends Component {
+  state = {
+   tech : "Redux"
+  }
+
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    return <HelloWorld tech={this.state.tech}/>
   }
 }
 
